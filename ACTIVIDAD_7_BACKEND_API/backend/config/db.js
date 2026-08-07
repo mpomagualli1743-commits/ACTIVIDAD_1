6861d038948e905f5f82d82b4c39cc9f4bdeaa26
mpomagualli1743-commits <mpomagualli1743@utm.edu.ec>
2026-08-07 13:02:17 -0500

    Eliminar carpeta ACTIVIDAD_7_BACKEND_API

--- ACTIVIDAD_7_BACKEND_API/backend/config/db.js
+++
@@ -1,9 +0,0 @@
-const { Pool } = require("pg");
-const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "helpdesk_db",
-    password: "08Noviembre",
-    port: 5432
-});
-module.exports = pool;
